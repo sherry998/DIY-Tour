e64cc163d680e8b1550f2891862b405c2eb0be87
TINAYAO <Juvia333@hotmail.com>
2018-03-26 22:37:52 +1000

    Fully check user input (espically if the email already exists) and only allow submittion if everything is good.

--- javascript/checkInput.js
+++ javascript/checkInput.js
@@ -1,44 +1,92 @@
+var emailState = false;
+var passwordState = false;
+var usernameState = false;
+var label='';
+
 $(document).ready(function() {
+
 	$('input[type="text"], input[type="password"]').on('keyup', function() {
-		var name = $(this).attr("name");
-		var value = document.getElementsByName(name)[0].value;
+
+		var id = $(this).attr('id');
+		var value = document.getElementById(id).value;
 		// remove all white space
 		value = value.replace(/\s/g, '');
-		var labelName = name+'Label';
+		var labelName = id+'Label';
 		
 		if (value !== null && value !== ''){
-			// call function base on the current input
-			var functionName = "check"+name;
+			// call function base on current input
+			var functionName = "check"+id;
 			var labelText = window[functionName](value);
-			document.getElementById(labelName).innerHTML = labelText;
 		}else{
-			document.getElementById(labelName).innerHTML = name+" is missing";
+			document.getElementById(labelName).innerHTML = id+" is missing";
 		}
+		});
+		
+	});
+	
+function checkInput(){
+	console.log(emailState);
+	console.log(passwordState);
+	console.log(usernameState);
+	if (emailState==true && passwordState==true && usernameState==true){
+		var email = $('#Email').val();
+		var username = $('#Username').val();
+		var psw = $('#Password').val();
+		var input = email + ":" + username + ":" + psw;
 		$.ajax({
-			url: 'php/processInput.php',
-			success: function(){
-				// If successful run updataDisplay
-				//console.log("success");
-			},
-		error: function(){
+			url: 'php/createAccount.php',
+			type: 'post',
+			data: {"callCreateAccount":input},
+		success: function(data){
+			var modal = document.getElementById('myModal');
+			document.getElementById("modalText").innerHTML = "Welcome, please check your email: " + email+" to activate your account";
+			modal.style.display = "block";
+		},
+		error: function(data){
 			console.log("error");
 		}
 		});
-	});
-});
+		
+	} else {
+		// css effects to be added onto the label
+		console.log('incorrect');
+	}
+}	
 
 function checkEmail(email){
 	//https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	var label='';
 	if (!re.test(email)){
-		label = "Invaild email address"
+		emailState = false;
+		document.getElementById("EmailLabel").innerHTML = "Invaild email address!";
+	} else {
+		// ajax asynchronous return, therefore label is changed within it
+		$.ajax({
+		url: 'php/createAccount.php',
+		type: 'post',
+		data: {"callCheckEmail":email},
+		success: function(data){
+			console.log(data);
+			if (data=="true"){
+				emailState = true;
+				document.getElementById("EmailLabel").innerHTML = "";
+			} else {
+				emailState = false;
+				document.getElementById("EmailLabel").innerHTML = "Email already exists";
+				console.log(label);
+			}
+		},
+		error: function(data){
+			console.log("Error connecting to database");
+		}
+		});
 	}
-	return(label);
+	
 }
 
-function checkUsername(){
-	return("");
+function checkUsername(uname){
+	usernameState=true;
+	document.getElementById("UsernameLabel").innerHTML = "";
 }
 
 function checkPassword(psw){
@@ -46,14 +94,18 @@ function checkPassword(psw){
 	var strongRegex = new RegExp("(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
 	var mediumRegex = new RegExp("(?=.{8,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
 	var enoughRegex = new RegExp("(?=.{8,}).*", "g");
+	var label = "";
+	passwordState= true;
 	
 	if (false == enoughRegex.test(psw)) {
-		return("Your password must contain 8 characters");
+		passwordState= false;
+		label ="Your password must contain 8 characters";
 	} else if (strongRegex.test(psw)) {
-		return("Strong!");
+		label ="Strong!";
 	} else if (mediumRegex.test(psw)) {
-		return("Medium!");
+		label = "Medium!";
 	} else {
-		return("Weak!");
+		label = "Weak!";
 	}
+	document.getElementById("PasswordLabel").innerHTML = label;
 }
--- php/createAccount.php
+++ php/createAccount.php
@@ -1,21 +1,41 @@
 <?php
+	
 	$mysqli = new mysqli('localhost', 'root', '', 'diy_tour');
 	
 	if ($mysqli->connect_error) {
-		echo("Connection failed: " . $conn->connect_error);
-	} 
+		echo("Connection failed: " . $mysqli->connect_error);
+	}
 	
-	$email = $_POST['email']; 
-	$uname = $_POST['uname']; 
-	$psw = $_POST['psw']; 
+	if (isset($_POST['callCheckEmail'])) {
+        checkEmail($_POST['callCheckEmail'],$mysqli);
+    }
 	
-	$sql = "INSERT INTO account (email, userName, password)
-	VALUES ('$email', '$uname', '$psw')";
+	if (isset($_POST['callCreateAccount'])) {
+		$inputArray = explode(':', $_POST['callCreateAccount']);
+        createAccount($inputArray[0],$inputArray[1],$inputArray[2],$mysqli);
+    }
 	
-	if ($mysqli->query($sql) === TRUE) {
-		echo "Welcome, please check your email: " .$email . " to activate your account";
-	} else {
-		echo "Error: " . $sql . "<br>" . $mysqli->error;
+	function checkEmail($email,$mysqli){
+		$query = mysqli_query($mysqli,"SELECT * FROM account WHERE email = @'$email'");
+		
+		if(mysqli_num_rows($query) == 0){
+			echo "true";
+		} else {
+			echo "false";
+		}	
+	}
+
+	
+	function createAccount($email,$uname,$psw,$mysqli){
+		
+		$sql = "INSERT INTO account (email, userName, password)
+			VALUES (@'$email', @'$uname', @'$psw')";
+			
+		if ($mysqli->query($sql) === TRUE) {
+			echo "Welcome, please check your email: " .$email . " to activate your account";
+		} else {
+			echo "Error: " . $sql . "<br>" . $mysqli->error;
+		}
 	}
 	
 	$mysqli->close();
