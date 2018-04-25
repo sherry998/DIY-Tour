b3f4f854569e444cadff0944689f1bc14558c479
LINGYUN XIE <l.xie@uq.net.au>
2018-04-25 11:36:33 +1000

    INCLUDE/Load header.html, footer.html, cpryright.html, sideNav.html USING JQUERY

---
+++ javascript/loadHtml.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){ 
+  $.get("header.html", function(data) {
+    $("#header").html(data);
+  });
+  
+  
+  $.get("footer.html", function(data) {
+    $("#footer").html(data);
+  });
+  
+  $.get("copyright.html", function(data) {
+    $("#copyright").html(data);
+  });
+  
+  $.get("sideNav.html", function(data) {
+    $("#sideNav").html(data);
+  });
+});
+
+
